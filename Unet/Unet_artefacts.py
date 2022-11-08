9f8262e2390450b449b83ca9c3d35fc59f9c6583
Filippo Gatti <filippo.gatti@centralesupelec.fr>
2022-11-08 19:44:31 +0100

    [BUGFIX] read raw images correctly

--- Unet/Unet_artefacts.py
+++ Unet/Unet_artefacts.py
@@ -5,20 +5,19 @@ import Unet_utils
 import Unet_model
 import Unet_preprocess
 import Unet_postprocess
-# import tensorflow as tf
-
 
 if __name__=="__main__":
     
+    # Parse options
     opt = Unet_utils.Parse()
-
+    
+    # Define device
     device_name = Unet_model.tf.test.gpu_device_name()
     if device_name != '/device:GPU:0':
         raise SystemError('GPU device not found')
         print('Found GPU at: {}'.format(device_name))
 
-    Unet = Unet_model.Unet(**opt)
-
+    # Define databases
     trDatabaseList = Unet_utils.getPath(database=opt["database"],folder=opt["trDatabase"])
     tsDatabaseList = Unet_utils.getPath(database=opt["database"],folder=opt["tsDatabase"])
     vdDatabaseList = Unet_utils.getPath(database=opt["database"],folder=opt["vdDatabase"])
@@ -26,7 +25,6 @@ if __name__=="__main__":
     X_train, recon_X_train = Unet_preprocess.getImgs(trDatabaseList, mode = 0, **opt)
     Y_train, _ = Unet_preprocess.getImgs(trDatabaseList, mode = 1, **opt)
     X_test, recon_X_test = Unet_preprocess.getImgs(tsDatabaseList, mode = 0, **opt)
-
 
     h5f = Unet_preprocess.h5py.File('TrainData.h5','w')
     h5f.create_dataset('image', data=X_train)
@@ -40,11 +38,17 @@ if __name__=="__main__":
     h5f.create_dataset('image', data=X_test)
     h5f.close()
 
+    # Initialize Unet
+    Unet = Unet_model.Unet(**opt)
+    
     # Fit model
     earlystopper = Unet_model.EarlyStopping(patience=15, verbose=1)
     checkpointer = Unet_model.ModelCheckpoint("model_unet_checkpoint.h5", verbose=1, save_best_only=True)
-    history = Unet.fit(X_train, Y_train, validation_split=0.1, 
-                       batch_size=1, epochs=opt["epochs"],
+    history = Unet.fit(X_train, 
+                       Y_train, 
+                       validation_split=0.1, 
+                       batch_size=1, 
+                       epochs=opt["epochs"],
                        callbacks=[checkpointer])
     Unet.save("{:>s}/Unet.h5".format(opt["database"]))
 
