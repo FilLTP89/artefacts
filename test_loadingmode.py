5d192806ce91282333ad64d9d4b88d8d8b2b7d17
Yuuuugo <hugogabrielidis@gmail.com>
2023-02-12 18:46:42 +0100

    [TESTING] testing vgg on valid ds

--- test_loadingmode.py
+++ test_loadingmode.py
@@ -5,7 +5,12 @@ from data_file.processing_vgg import VGGDataset
 
 if __name__ == "__main__":
     model = VGG19(classifier_training=True)
-    model.load_weights("model/saved_models/VGG19/big_endian/cp.cpkt")
+    model.build(input_shape = (None,512,512,1))
+    model.load_weights("model/saved_models/VGG19/low_endian/cp.ckpt")
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=3e-4),
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+            metrics=tf.keras.metrics.SparseCategoricalAccuracy(),
+            )
     dataset = VGGDataset(
         height=512,
         width=512,
@@ -13,5 +18,5 @@ if __name__ == "__main__":
         big_endian =True
     )
     dataset.setup()
-    _, _, test_ds = dataset.train_ds, dataset.valid_ds, dataset.test_ds
-    model.evaluate(test_ds)
+    _, valid_ds, test_ds = dataset.train_ds, dataset.valid_ds, dataset.test_ds
+    model.evaluate(valid_ds)
