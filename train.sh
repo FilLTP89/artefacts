94b9fbf5dd461b15fd7312ac5735e3fc37a597d0
Yuuuugo <hugogabrielidis@gmail.com>
2023-03-16 14:24:52 +0100

    [TRY] try to train deepmar

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=artefacts
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:3
 #SBATCH --mem=80GB
 #SBATCH --output=ruche_log/output.txt
 #SBATCH --error=ruche_log/error.txt
@@ -19,5 +19,5 @@ source $WORKDIR/launch_script.sh
 #python3 train.py --model VGG19 --epochs 20 --batch_size 32 --wandb --saving_path "model/saved_models/VGG19/" --dicom --learning_rate 1.5e-4
 #python3 train.py --model MedGAN --epochs 20 --batch_size 3 --wandb --saving_path "model/saved_models/MedGAN/" --dicom
 #python3 train.py --model ResUnet --epochs 20 --batch_size 8 --wandb --saving_path "model/saved_models/ResUnet/" --big_endian
-python3 train.py --model MedGAN --epochs 5 --batch_size 5 --dicom 
-#python3 train.py --model DeepMAR --epochs 5 --batch_size 3 --dicom 
+#python3 train.py --model MedGAN --epochs 5 --batch_size 5 --dicom 
+python3 train.py --model DeepMAR --epochs 5 --batch_size 10 --dicom 
