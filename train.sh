a6ba77dbf92b60503f2ddd2d88e428f9d21e2399
Yuuuugo <hugogabrielidis@gmail.com>
2023-03-20 20:34:24 +0100

    [CHANGE] make sure lr is 2e-6

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=artefacts
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:3
 #SBATCH --mem=80GB
 #SBATCH --output=ruche_log/output.txt
 #SBATCH --error=ruche_log/error.txt
@@ -17,7 +17,7 @@ module purge
 source $WORKDIR/launch_script.sh
 #cd $WORKDIR/artefacts
 #python3 train.py --model VGG19 --epochs 20 --batch_size 32 --wandb --saving_path "model/saved_models/VGG19/" --dicom --learning_rate 1.5e-4 --save_weights
-python3 train.py --model MedGAN --epochs 20 --batch_size 3 --wandb --saving_path "model/saved_models/MedGAN/" --dicom --save_weights
+python3 train.py --model MedGAN --epochs 20 --batch_size 3 --wandb --saving_path "model/saved_models/MedGAN/" --dicom --learning_rate 2e-6 --save_weights 
 #python3 train.py --model ResUnet --epochs 20 --batch_size 8 --wandb --saving_path "model/saved_models/ResUnet/" --big_endian
 #python3 train.py --model MedGAN --epochs 5 --batch_size 3 --dicom 
 #python3 train.py --model DeepMAR --epochs 5 --batch_size 1 --dicom 
