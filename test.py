0a0dabf4e2589dfed4cfd0c05e018f12974e1850
Yuuuugo <hugogabrielidis@gmail.com>
2023-03-23 09:30:47 +0100

    [CHANGE] change medgan path in test

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ def load_model(model_path=None):
 
 
 def load_model_with_weights(
-    model_path="model/saved_models/MedGAN/dicom/grateful-sunset-15/17/model.ckpt",
+    model_path="artefacts/model/saved_models/MedGAN/dicom/floral-flower-18/20/model.ckpt",
 ):
     model = MEDGAN()
     model.build(input_shape=(None, 512, 512, 1))
