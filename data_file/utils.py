5443c30ae6368124389954b43aa01a5c2a5cc8f1
Yuuuugo <hugogabrielidis@gmail.com>
2023-04-05 16:01:48 +0200

    [CHANGE] save in tif format

--- data_file/utils.py
+++ data_file/utils.py
@@ -10,7 +10,7 @@ def save_file(
     y,
     name,
     path="images/generated_images/",
-    extension=".jpg",
+    extension=".tif",
     brightness_fact=3,
     big_endian=False,
     dicom=False,
