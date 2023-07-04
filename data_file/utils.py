0688bfc1372d54350a971a0798e1dd2bd40c4767
Yuuuugo <hugogabrielidis@gmail.com>
2023-07-04 17:10:05 +0200

    [CHANGE] reduce the number of parameter in the metadata

--- data_file/utils.py
+++ data_file/utils.py
@@ -6,63 +6,6 @@ import SimpleITK as sitk
 import os
 import cv2
 
-def write_raw(array,
-             file_name, 
-             image_size=(400, 400), 
-             sitk_pixel_type=sitk.sitkUInt16, 
-             image_spacing=None, 
-             image_origin=None, 
-             big_endian=True, 
-             umin=137, 
-             umax=52578):
-
-
-    # Scale array back to the original range
-    array = (array * (umax - umin)) + umin
-    array = array.astype(np.uint16)  # convert back to uint16 (based on sitk_pixel_type)
-
-    # Write raw file
-    with open(file_name + '.raw', 'wb') as f:
-        if big_endian:
-            f.write(array.astype('>u2').tobytes())
-        else:
-            f.write(array.astype('<u2').tobytes())
-    
-    # Convert numpy array to sitk image
-    img = sitk.GetImageFromArray(array)
-    img.SetSpacing(image_spacing if image_spacing else [1]*len(image_size))
-    img.SetOrigin(image_origin if image_origin else [0]*len(image_size))
-
-    # Prepare metadata dictionary
-
-    direction_cosine = [
-    "1 0 0 1",  # 2D identity matrix
-    "1 0 0 0 1 0 0 0 1",  # 3D identity matrix
-    "1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1"  # 4D identity matrix
-    ]
-    meta_dict = {
-    'ObjectType': 'Image',
-    'NDims': str(len(image_size)),
-    'DimSize': ' '.join(map(str, image_size)),
-    'ElementSpacing': ' '.join(map(str, image_spacing)) if image_spacing else ' '.join(['1'] * len(image_size)),
-    'Offset': ' '.join(map(str, image_origin)) if image_origin else ' '.join(['0'] * len(image_size)),
-    'TransformMatrix': direction_cosine[len(image_size) - 2],
-    'ElementType': 'MET_USHORT',  # hardcoded as we're converting array to uint16
-    'BinaryData': 'True',
-    'BinaryDataByteOrderMSB': str(big_endian),
-    'ElementDataFile': os.path.abspath(file_name) + '.raw',
-}
-
-    # Add metadata to the image
-    for k, v in meta_dict.items():
-        img.SetMetaData(k, v)
-
-    # Use ImageFileWriter to write the image
-    writer = sitk.ImageFileWriter()
-    writer.SetFileName(file_name + '.mhd')
-    writer.SetUseCompression(False)
-    writer.Execute(img)
-
 
 def save_file(
     x,
@@ -116,6 +59,63 @@ def save_file(
         dpi = 1
     )
 
+def write_raw(array,
+             file_name, 
+             image_size=(400, 400), 
+             image_spacing=None, 
+             image_origin=None, 
+             big_endian=True, 
+             umin=137, 
+             umax=52578):
+
+
+    # Scale array back to the original range
+    array = (array * (umax - umin)) + umin
+    array = array.astype(np.uint16)  # convert back to uint16 (based on sitk_pixel_type)
+
+    # Write raw file
+    with open(file_name + '.raw', 'wb') as f:
+        if big_endian:
+            f.write(array.astype('>u2').tobytes())
+        else:
+            f.write(array.astype('<u2').tobytes())
+    
+    # Convert numpy array to sitk image
+    img = sitk.GetImageFromArray(array)
+    img.SetSpacing(image_spacing if image_spacing else [1]*len(image_size))
+    img.SetOrigin(image_origin if image_origin else [0]*len(image_size))
+
+    # Prepare metadata dictionary
+    """
+    direction_cosine = [
+    "1 0 0 1",  # 2D identity matrix
+    "1 0 0 0 1 0 0 0 1",  # 3D identity matrix
+    "1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1"  # 4D identity matrix
+    ]
+    
+    meta_dict = {
+    'ObjectType': 'Image',
+    'NDims': str(len(image_size)),
+    'DimSize': ' '.join(map(str, image_size)),
+    'ElementSpacing': ' '.join(map(str, image_spacing)) if image_spacing else ' '.join(['1'] * len(image_size)),
+    'Offset': ' '.join(map(str, image_origin)) if image_origin else ' '.join(['0'] * len(image_size)),
+    'TransformMatrix': direction_cosine[len(image_size) - 2],
+    'ElementType': 'MET_USHORT',  # hardcoded as we're converting array to uint16
+    'BinaryData': 'True',
+    'BinaryDataByteOrderMSB': str(big_endian),
+    'ElementDataFile': os.path.abspath(file_name) + '.raw',
+    }
+    
+    # Add metadata to the image
+    for k, v in meta_dict.items():
+        img.SetMetaData(k, v)
+    """
+    # Use ImageFileWriter to write the image
+    writer = sitk.ImageFileWriter()
+    writer.SetFileName(file_name + '.mhd')
+    writer.SetUseCompression(False)
+    writer.Execute(img)
+
 
 def save_to_raw(
     x,
@@ -156,3 +156,16 @@ def save_to_raw(
        image_size=shape, 
        big_endian= True
    )
+    
+
+if __name__ == "__main__":
+    array = np.random.rand(400,400)
+    write_raw(
+        array= array,
+        file_name= "test",
+        image_size=(400,400)
+    )
+    data = sitk.ReadImage("test.mhd")
+    array = np.array(sitk.GetArrayFromImage(data))
+    plt.imshow(array, cmap ="gray")
+    plt.show()
