9887dce4e5d3f2db6e71c5fb2703fcb77a8a518e
Yuuuugo <hugogabrielidis@gmail.com>
2023-09-06 02:45:09 +0200

    check error matlab

--- matlab/script.sh
+++ matlab/script.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-
-#SBATCH --job-name=job-matlab
+#SBATCH --job-name=artefact_matlab
 #SBATCH --output=%x.o%j 
 #SBATCH --time=00:20:00 
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpua100
 #SBATCH --nodes=1 
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=8
 #SBATCH --partition=cpu_short
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+
 
 # Module load
 module purge
 module load matlab/R2020a/intel-19.0.3.199
-
 matlab -nodisplay -r read_images
