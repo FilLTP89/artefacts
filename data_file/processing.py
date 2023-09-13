[ADD] add mixed precision option in the train files / correct bug with medgan path

data_file/processing.py
@@ -235,7 +235,7 @@ class Dataset:
 
 if __name__ == "__main__":
     print("Generating sample ....")
-    dataset = Dataset(path="../data/", batch_size=1, big_endian=True)
+    dataset = Dataset(path="../data/", batch_size=20, big_endian=True, shuffle=True)
     dataset.setup()
     train_ds, valid_ds, test_ds = dataset.train_ds, dataset.valid_ds, dataset.test_ds
     """
@@ -250,7 +250,11 @@ if __name__ == "__main__":
             )
     """
     for idx , (x,y) in enumerate(train_ds):
-        nan_in_x = tf.math.reduce_any(tf.math.is_nan(x))
-        nan_in_y = tf.math.reduce_any(tf.math.is_nan(y))
-        if nan_in_x or nan_in_y:
-            print(f"nan in x : {nan_in_x} and nan in y : {nan_in_y} at index {idx}")
+        print(x.shape, y.shape)
+        print(x.dtype, y.dtype)
+        print(x.max(), y.max())
+        print(x.min(), y.min())
+        print()
+
+        if idx > 10:
+            break

train.py
@@ -79,12 +79,19 @@ def train(config):
         )
 
     config = wandb.config if config.wandb else config
+    if config.mix_precision:
+        print("Using mixed precision")
+        from tensorflow.keras import mixed_precision
+        policy = mixed_precision.Policy("mixed_float16")
+        mixed_precision.set_global_policy(policy)
+    else:
+        print("Not using mixed precision")
+        
     gpus = (
         tf.config.list_logical_devices("GPU")
         if len(tf.config.list_physical_devices("GPU")) > 0
         else 1
     )
-    print(f"Generating sample  with batch_size = {config.batch_size * len(gpus)}")
     if config.model == "VGG19":
         print("VGG19 Dataset")
         dataset = VGGDataset(
@@ -116,7 +123,6 @@ def train(config):
     print("Batch size :", config.batch_size * len(gpus))
     strategy = tf.distribute.MirroredStrategy(gpus)
     with strategy.scope():
-        print("Creating the model ...")
         model = Model(
             model_name=config.model,
             input_shape=config.img_size,
@@ -124,7 +130,6 @@ def train(config):
             big_endian=config.big_endian,
             pretrained_MedGAN=config.pretrained_MedGAN,
         ).build_model()
-        print("Model Created!")
 
     print("Start Training")
     if config.one_batch_training: