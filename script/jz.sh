6baffd0b4913a0be19f2aece2886fe848a800ee0
Yuuuugo <hugogabrielidis@gmail.com>
2024-09-01 21:49:41 +0200

    [ADD] python -> srun

--- script/jz.sh
+++ script/jz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Generate
+#SBATCH --job-name=artefact
 #SBATCH --output=jeanzay_log/output.txt
 #SBATCH --error=jeanzay_log/error.txt
 #SBATCH --constraint=a100
@@ -14,17 +14,15 @@
 
 module load anaconda-py3/2023.09
 module load cuda/11.8.0
-module load gcc/11.3.0
 
 export NCCL_P2P_DISABLE=1
-export WANDB_MODE=offline
 export LD_LIBRARY_PATH=/linkhome/rech/tui/upz57sx/.conda/envs/sismic/lib
 export  XLA_FLAGS="--xla_gpu_cuda_data_dir=/linkhome/rech/genuqo01/upz57sx/.conda/envs/artefact/lib"
 source activate artefact
+
 cd /lustre/fswork/projects/rech/xvy/upz57sx/artefact
 export PYTHONPATH="./"
-
-srun /lustre/fswork/projects/rech/xvy/upz57sx/artefact/pl_training.py \
+WANDB_MODE=offline python3 /lustre/fswork/projects/rech/xvy/upz57sx/artefact/pl_training.py \
     --max_epochs 100 \
     --train_bs 16 \
     --test_bs 16 \
