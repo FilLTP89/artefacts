3d15277688148d4ca71f3f6229b2b56c5d099634
Yuuuugo <hugogabrielidis@gmail.com>
2024-09-01 21:43:24 +0200

    [ADD] python -> srun

--- script/jz.sh
+++ script/jz.sh
@@ -16,7 +16,7 @@ module load anaconda-py3/2023.09
 module load cuda/11.8.0
 module load gcc/11.3.0
 
-
+export NCCL_P2P_DISABLE=1
 export WANDB_MODE=offline
 export LD_LIBRARY_PATH=/linkhome/rech/tui/upz57sx/.conda/envs/sismic/lib
 export  XLA_FLAGS="--xla_gpu_cuda_data_dir=/linkhome/rech/genuqo01/upz57sx/.conda/envs/artefact/lib"
@@ -24,7 +24,7 @@ source activate artefact
 cd /lustre/fswork/projects/rech/xvy/upz57sx/artefact
 export PYTHONPATH="./"
 
-python3 pl_training.py \
+srun /lustre/fswork/projects/rech/xvy/upz57sx/artefact/pl_training.py \
     --max_epochs 100 \
     --train_bs 16 \
     --test_bs 16 \
