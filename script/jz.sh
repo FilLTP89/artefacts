14a5d3c3571756ff001926e96b5392f1359bff61
Yuuuugo <hugogabrielidis@gmail.com>
2024-09-01 20:17:42 +0200

    [ADD] new jz script

--- script/jz.sh
+++ script/jz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=Generate
-#SBATCH --output=jeanzay_log/generate_output.txt
-#SBATCH --error=jeanzay_log/generate_error.txt
+#SBATCH --output=jeanzay_log/jz_output.txt
+#SBATCH --error=jeanzay_log/jz_error.txt
 #SBATCH --constraint=a100
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -17,14 +17,16 @@ module load cuda/11.8.0
 module load gcc/11.3.0
 
 
-
 WANDB__SERVICE_WAIT=1000
 export LD_LIBRARY_PATH=/linkhome/rech/tui/upz57sx/.conda/envs/sismic/lib
 export  XLA_FLAGS="--xla_gpu_cuda_data_dir=/linkhome/rech/tui/upz57sx/.conda/envs/sismic/lib"
 source activate sismic
-cd /gpfswork/rech/tui/upz57sx/dossier_hugo/sftp_alice
+cd /lustre/fswork/projects/rech/xvy/upz57sx/artefact
 export PYTHONPATH="./"
-python3 generate/generate_small_dance_diffusion.py
 
+srun pl_training.py \
+    --max_epochs 100 \
+    --train_bs 16 \
+    --test_bs 16 \
 
 
