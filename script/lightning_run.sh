4559848fe721af8be5a90b188bad0350b396ca06
Yuuuugo <hugogabrielidis@gmail.com>
2024-09-01 17:40:08 +0200

    [CHANGE] temporaly disallow mix_precision as it gives nan errors

--- script/lightning_run.sh
+++ script/lightning_run.sh
@@ -29,7 +29,6 @@ python3 pl_training.py \
     --max_epochs 100 \
     --train_bs 16 \
     --test_bs 16 \
-    --mix_precision \
 
 
 
--- script/multi_gpu.sh
+++ script/multi_gpu.sh
@@ -52,7 +52,6 @@ srun /gpfs/users/gabrielihu/.conda/envs/artefact/bin/python pl_training.py \
     --max_epochs 100 \
     --train_bs 16 \
     --test_bs 16 \
-    --mix_precision \
 
 echo "Job script completed"
 
