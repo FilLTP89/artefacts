f917d74154d69975d2c51d654047d7f7cf900dda
Yuuuugo <hugogabrielidis@gmail.com>
2024-09-02 11:02:12 +0200

    [ADD] clarification

--- pl_training.py
+++ pl_training.py
@@ -50,10 +50,9 @@ def init_wandb():
 def init_repo(wandb_name, ruche = False):
     if not ruche:
         path = f"model/saved_model/{wandb_name}"
+        os.makedirs(path)
     else:
         path = f"/gpfs/users/gabrielihu/saved_model/{wandb_name}"
-    if not os.path.exists(path):
-        os.makedirs(path)
     return path
 
 
