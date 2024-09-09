001a1cc00b32ffbf9db6bfba0c1bfb39580c03e0
Yuuuugo <hugogabrielidis@gmail.com>
2024-09-09 14:04:42 +0200

    mix precision and 14->15

--- script/multi_gpu.sh
+++ script/multi_gpu.sh
@@ -55,7 +55,7 @@ srun /gpfs/users/gabrielihu/.conda/envs/artefact/bin/python pl_training.py \
     --no-use_feature_extractor \
     --task="GAN" \
     --resume_from_cpkt \
-    --mixed_precision \
+    --mix_precision \
 
 echo "Job script completed"
 
