431c1be9be175057683a446925b3d8fb8fca937c
Yuuuugo <hugogabrielidis@gmail.com>
2024-09-09 12:21:59 +0200

    [ADD] no args and kwargs in this case

--- pl_training.py
+++ pl_training.py
@@ -83,7 +83,7 @@ def load_model(task ="GAN",
     if task == "GAN":
         if resume_from_cpkt:
             vgg = VGG19(classifier_training= True, n_class=14)
-            model = AttentionMEDGAN(feature_extractor = vgg,*args, **kwargs)
+            model = AttentionMEDGAN(feature_extractor = vgg)
         else: 
             model = AttentionMEDGAN(*args, **kwargs)
     else:
