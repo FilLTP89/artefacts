4b67fed87429a7f69060812cd720461203ec8b75
Yuuuugo <hugogabrielidis@gmail.com>
2024-10-02 15:44:51 +0200

    small update

--- script/jeanzay/singularity_jeanzay.sh
+++ script/jeanzay/singularity_jeanzay.sh
@@ -15,7 +15,7 @@
 cd /lustre/fswork/projects/rech/xvy/ucn85lb/artefacts/
 module load singularity/3.8.5
 
-command_to_run="srun WANDB_MODE=offline python3 pl_training.py \
+command_to_run="WANDB_MODE=offline python3 pl_training.py \
     --max_epochs 100 \
     --train_bs 3 \
     --test_bs 3 \
@@ -27,4 +27,4 @@ command_to_run="srun WANDB_MODE=offline python3 pl_training.py \
 
 start_container_cmd="singularity exec --pwd /lustre/fswork/projects/rech/xvy/ucn85lb/artefacts/ -B /lustre/fswork/projects/rech/xvy/ucn85lb:/lustre/fswork/projects/rech/xvy/ucn85lb/ --bind /lustre/fswork/projects/rech/xvy/ucn85lb/artefacts:/lustre/fswork/projects/rech/xvy/ucn85lb/artefacts --nv /lustre/fsn1/singularity/images/ucn85lb/lightning_latest.sif"
 
-$start_container_cmd /bin/bash -c "$command_to_run"
+srun $start_container_cmd /bin/bash -c "$command_to_run"
