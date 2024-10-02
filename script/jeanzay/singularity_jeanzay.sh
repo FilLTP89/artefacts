da7851c20a01429fcab6f4d1b3f76f939756f6cc
Yuuuugo <hugogabrielidis@gmail.com>
2024-10-02 11:56:30 +0200

    small update

--- script/jeanzay/singularity_jeanzay.sh
+++ script/jeanzay/singularity_jeanzay.sh
@@ -16,15 +16,15 @@
 cd /lustre/fswork/projects/rech/xvy/ucn85lb/artefacts/
 
 start_container_cmd='singularity exec --pwd /lustre/fswork/projects/rech/xvy/ucn85lb -B /lustre/fswork/projects/rech/xvy/ucn85lb:/lustre/fswork/projects/rech/xvy/ucn85lb/ --bind /lustre/fswork/projects/rech/xvy/ucn85lb/artefacts:/lustre/fswork/projects/rech/xvy/ucn85lb/artefacts --nv /lustre/fsn1/singularity/images/ucn85lb/lightning_latest.sif'
-command_to_run='WANDB_MODE=offline python3 pl_training.py \
+command_to_run='bash -c "WANDB_MODE=offline python3 pl_training.py \
     --max_epochs 100 \
     --train_bs 3 \
     --test_bs 3 \
     --ruche \
     --no-use_feature_extractor \
-    --task="Conditional_Diffusion" \
+    --task=Conditional_Diffusion \
     --mix_precision \
-    --data_folder="control"
-    --accumulate_grad_batches 4'
+    --data_folder=control \
+    --one_batch"'
 
 $start_container_cmd $command_to_run
