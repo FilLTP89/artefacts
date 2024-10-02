137ff04794de628572e4a1a6885bed51d82d269e
Yuuuugo <hugogabrielidis@gmail.com>
2024-10-02 16:55:16 +0200

    small update

--- model/torch/DIffusion_UNET.py
+++ model/torch/DIffusion_UNET.py
@@ -177,9 +177,9 @@ class ImageToImageDDIMLightningModule(pl.LightningModule):
             layers_per_block=2,
             block_out_channels=(128, 128, 256, 256, 512, 512),
             down_block_types=(
+                "AttnDownBlock2D",
                 "CrossAttnDownBlock2D",
-                "CrossAttnDownBlock2D",
-                "CrossAttnDownBlock2D",
+                "AttnDownBlock2D",
                 "DownBlock2D",
                 "AttnDownBlock2D",
                 "DownBlock2D",
