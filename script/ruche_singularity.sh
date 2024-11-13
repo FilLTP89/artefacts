34837a61bcbd9144310ac6931726f555c62ac0bc
Yuuuugo <hugogabrielidis@gmail.com>
2024-11-13 13:46:59 +0100

    [ADD] correct sh file

--- script/ruche_singularity.sh
+++ script/ruche_singularity.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=artefact
+#SBATCH --job-name=SDD
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
-#SBATCH --mem=40GB
-#SBATCH --output=ruche_log/output.txt
-#SBATCH --error=ruche_log/error.txt
+#SBATCH --mem=80GB
+#SBATCH --output=ruche_log/singularity_output.txt
+#SBATCH --error=ruche_log/singularity_error.txt
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=8
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=4
 #SBATCH --mail-type=FAIL
 #SBATCH --partition=gpua100
 #SBATCH --export=NONE
 #SBATCH --exclude=ruche-gpu16,ruche-gpu13,ruche-gpu11
 
-module load singularity/3.8.3/gcc-11.2.0
 cd $WORKDIR
+module load singularity/3.8.3/gcc-11.2.0
 start_container_cmd="singularity exec --pwd /gpfs/workdir/gabrielihu -B /gpfs/workdir:/gpfs/workdir --bind /home/${USER}/:/home/${USER}/ --bind /gpfs/workdir/gabrielihu/artefacts:/gpfs/users/gabrielihu/artefacts --nv lightning_latest.sif"
 
 
@@ -49,4 +49,4 @@ VGG_COMMAND="WANDB_MODE=offline python3 pl_training.py \
 
 
 
-srun $start_container_cmd /bin/bash -c "$VGG_COMMAND"
+$start_container_cmd /bin/bash -c "$VGG_COMMAND"
