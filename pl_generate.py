46e66e5739517c486178c23bcfb9072e277afd00
Yuuuugo <hugogabrielidis@gmail.com>
2024-11-13 11:34:02 +0100

    small update

--- pl_generate.py
+++ pl_generate.py
@@ -37,6 +37,7 @@ def generate_images(model,
     for idx_, (input,target,input_path,target_path) in enumerate(ds):
         input = input.to(device)
         with torch.no_grad():
+            print(input.unsqueeze(0).shape)
             generated = model(input.unsqueeze(0))   
             generated = generated.squeeze(0) 
             generated = generated.cpu().detach().numpy()
