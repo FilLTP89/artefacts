90870ca74763381dfa1caa40d1ae732d91dbc908
Yuuuugo <hugogabrielidis@gmail.com>
2024-11-18 05:14:18 +0100

    small update

--- data_file/processing_newdata.py
+++ data_file/processing_newdata.py
@@ -511,7 +511,7 @@ class LoadOneAcquisition(Dataset):
         os.makedirs(f"{output_dir}/input", exist_ok=True)
         os.makedirs(f"{output_dir}/target", exist_ok=True)
         os.makedirs(f"{output_dir}/generated", exist_ok=True)
-        
+
         for idx in range(len(self)):
             input_path, target_path = self.folder[idx]
             input_tensor, target_tensor, (input_range, target_range) = self[idx]
@@ -521,7 +521,7 @@ class LoadOneAcquisition(Dataset):
             
             input_arr = self.denormalize(input_tensor.squeeze().numpy(), input_range)
             target_arr = self.denormalize(target_tensor.squeeze().numpy(), target_range)
-            generated_arr = self.denormalize(generated_tensor.squeeze().cpu().numpy(), target_range)
+            generated_arr = self.denormalize(generated_tensor.squeeze().detach().numpy(), target_range)
             # Save DICOM
             for arr, orig_path, prefix in [(input_arr, input_path, 'input'), 
                                          (target_arr, target_path, 'target'),
