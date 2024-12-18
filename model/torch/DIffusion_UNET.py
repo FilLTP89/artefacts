f5e342f0a54bb61ec7311a37ecdc9fdae3f8c1dd
Yuuuugo <hugogabrielidis@gmail.com>
2024-12-18 15:03:49 +0100

    small update

--- model/torch/DIffusion_UNET.py
+++ model/torch/DIffusion_UNET.py
@@ -31,7 +31,7 @@ class StableDiffusionVQVQAE(pl.LightningModule):
                  *args, **kwargs):
         super().__init__(*args, **kwargs)
         model = AutoencoderKL.from_pretrained(
-            pretrained_model_name_or_path="/lustre/fswork/projects/rech/xvy/ucn85lb/medical_project/artefacts/model/vae/"
+            pretrained_model_name_or_path="/lustre/fswork/projects/rech/xvy/ucn85lb/medical_project/artefacts/model/vae/",
                 use_safetensors=True,
                 revision="main",
                 local_files_only=True  # Force download if needed
