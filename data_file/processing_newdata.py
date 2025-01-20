240180ec33ddca39ef6b1b092818f70f9844dd53
Yuuuugo <hugogabrielidis@gmail.com>
2025-01-20 01:35:02 +0100

    small update

--- data_file/processing_newdata.py
+++ data_file/processing_newdata.py
@@ -158,6 +158,9 @@ def load_one_acquisition(path = "datav2/protocole_1/",
     folder_name = f"{control}/{acquisition}/{dcm}/Input/{categorie}"
     print(f"Using folder: {folder_name}")
     print(f"Dataset size: {len(dataset)}")
+    for item in dataset:
+            print(item[0])
+            break
     acquisition = [item for item in dataset if folder_name in item[0]]
     print(f"Acquisition size: {len(acquisition)}")
     return acquisition
