a0ab90b03a0e06240dc64d9dfa07e143ad195a48
Yuuuugo <hugogabrielidis@gmail.com>
2025-01-21 14:25:54 +0100

    small update

--- pl_generate.py
+++ pl_generate.py
@@ -24,9 +24,9 @@ def load_model(checkpoint_path, device, *args, **kwargs):
 def main():
     print("Start")
     i = 0
-    acquisition_number = 4
+    acquisition_number = 3
     dcm = "dcm"
-    control = "control"
+    control = "fracture"
     categories = os.listdir(f"datav2/protocole_1/{control}/{acquisition_number}/{dcm}/Input/")
     print(f"Categorie : {categories}")
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
