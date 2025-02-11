00a905ad2ab48636e04d1528140c08d453ddb4be
Yuuuugo <hugogabrielidis@gmail.com>
2025-02-11 11:42:34 +0100

    small update

--- pl_generate.py
+++ pl_generate.py
@@ -24,7 +24,7 @@ def load_model(checkpoint_path, device, *args, **kwargs):
 def main():
     print("Start")
     i = 0
-    acquisition_number = 3
+    acquisition_number = 4
     dcm = "dcm"
     control = "fracture"
     categories = os.listdir(f"datav2/protocole_1/{control}/{acquisition_number}/{dcm}/Input/")
