64a03d8f07ba54aab62e319f9462a03c65b406eb
Yuuuugo <hugogabrielidis@gmail.com>
2025-03-17 16:26:23 +0100

    correct singularity command & sif path

--- pl_training.py
+++ pl_training.py
@@ -125,7 +125,7 @@ def load_module(
     if task == "Classification":
         module = Datav2Module(dataset_type = ClassificationDataset, data_folder= data_folder,*args, **kwargs)
     elif task == "old":
-        module = MetalArtifactDataModule( train_bs = train_bs,
+        module = MetalArtifactDataModule(train_bs = train_bs,
                                          test_bs = test_bs,
                                          *args, 
                                          img_size=img_size,
